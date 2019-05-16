be66c075503103217b776a65d7a034c363272a18
simivalley <peopletruthbetold@gmail.com>
2019-05-15 17:28:21 -0700

    working fine

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
@@ -20,8 +20,8 @@ public class Application {
     }
 
     @Bean
-    public ServletRegistrationBean actionServletRegistration(MovieServlet actionServlet) {
-        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
+    public ServletRegistrationBean actionServletRegistration(MovieServlet movieServlet) {
+        return new ServletRegistrationBean(movieServlet, "/moviefun/*");
     }
 
     @Bean
